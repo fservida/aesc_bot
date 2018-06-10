35c88ad43c213f424107f1f403df3342b62e8c55
Francesco Servida <francesco.servida@gmail.com>
2018-06-10 21:43:32 +0200

    Fixed testing unit

--- aesc_bot_testing/commands.py
+++ aesc_bot_testing/commands.py
@@ -4,11 +4,10 @@
 # Created by "Francesco Servida"
 # Created on 10.06.18
 
-from aesc_bot.commands import parse_menu, format_menu
+from aesc_bot.commands import parse_menu
 
 
 def test_menu(cantine = "amphimax"):
-    assiettes = parse_menu(cantine)
-    formatted_menu = format_menu(assiettes)
+    formatted_menu = parse_menu(cantine)
     if formatted_menu == "":
         raise ValueError("The formatted menu is an empty string")
